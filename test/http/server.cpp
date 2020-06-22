691be9e8f08e529a38f70d21e316178b474c7a2a
Tristan <code@thewoosh.me>
2020-06-23 01:03:45 +0200

    [Test] (HTTP) Add Server Test

---
+++ test/http/server.cpp
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include <gtest/gtest.h>
+
+#define TESTING
+
+#include "http/server.hpp"
+
+namespace HTTP {
+
+	Configuration config;
+
+	TEST(Server, CloseSocket) {
+		Server server(config);
+		server.internalSocket = 10;
+
+		server.CloseSocket();
+
+		ASSERT_EQ(server.internalSocket, -1);
+	}
+
+
+} // namespace HTTP
+
+int
+main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
