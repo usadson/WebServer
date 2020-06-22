8378f10dc4a054a4270b7e0b6be11f91a01a749c
Tristan <code@thewoosh.me>
2020-06-22 21:25:55 +0200

    [HTTP] (Configuration) Add pollAcceptTimeout

--- http/configuration.hpp
+++ http/configuration.hpp
@@ -17,6 +17,14 @@ struct Configuration {
 	// The amount of clients awaiting in the accept() queue
 	std::size_t listenerBacklog;
 
+	// The amount of time should pass between poll() calls to the main server
+	// socket.
+	//
+	// Time is in milliseconds.
+	//
+	// This is not really a performance issue, more a preference issue.
+	int pollAcceptTimeout{ 1000 };
+
 	uint16_t port;
 
 	bool useTransportSecurity;
