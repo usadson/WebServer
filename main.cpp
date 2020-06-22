ace153cbfb8e6fb0a7822f9c75a625a9f2ba181a
Tristan <code@thewoosh.me>
2020-06-22 17:08:00 +0200

    Some files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include <cstdlib>
+
+int
+main() {
+	return EXIT_SUCCESS;
+}
