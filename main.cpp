6d8bf6f9e9e1bb5aad548383d6ee91affcd43bcf
Tristan <code@thewoosh.me>
2020-06-22 23:50:22 +0200

    [main] Test logger

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #include <cstdlib>
 
+#include "base/logger.hpp"
 #include "http/configuration.hpp"
 #include "http/server.hpp"
 
@@ -19,11 +20,16 @@ main() {
 	HTTP::Server server(config);
 	server.Start();
 
+	Logger::Info("Main", "Server Started");
+
 	std::string test;
 	std::cin >> test;
+
+	Logger::Info("Main", "Stopping...");
 
 	server.SignalShutdown();
 	server.Join();
 
+	Logger::Info("Main", "Stopped!");
 	return EXIT_SUCCESS;
 }
