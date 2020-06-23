46243cade69236cf101caaf50cf25e4005b67f3a
Tristan <code@thewoosh.me>
2020-06-23 20:11:24 +0200

    [HTTP] (Utils) Add documentation

--- http/utils.hpp
+++ http/utils.hpp
@@ -11,11 +11,22 @@ namespace HTTP::Utils {
 	static const std::array unreservedCharacters =
 		{ '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~' };
 
+	// The HTTP specification states that we should parse octets as code points
+	// part of the USASCII subset.
+	//
+	// References:
+	// https://svn.tools.ietf.org/svn/wg/httpbis/specs/rfc7230.html#rfc.section.3.p.3
+	// http://sliderule.mraiow.com/w/images/7/73/ASCII.pdf
 	[[nodiscard]] inline constexpr bool
 	IsNonUSASCIICharacter(char character) noexcept {
 		return static_cast<uint8_t>(character) & 0x80;
 	}
 
+	// Some inputs are specified as 'token' definitions. This function makes
+	// sure the characters follow the standard.
+	//
+	// Reference:
+	// https://svn.tools.ietf.org/svn/wg/httpbis/specs/rfc7230.html#field.components
 	[[nodiscard]] inline constexpr bool
 	IsTokenCharacter(char character) {
 		if (IsNonUSASCIICharacter(character))
@@ -36,6 +47,11 @@ namespace HTTP::Utils {
 		return false;
 	}
 
+	// The path of a request is of request-target type. This function ensures
+	// that read characters are conforming to the definition of such a type.
+	//
+	// Reference:
+	// https://svn.tools.ietf.org/svn/wg/httpbis/specs/rfc7230.html#request-target
 	[[nodiscard]] inline constexpr bool
 	IsPathCharacter(char character) {
 		if (IsNonUSASCIICharacter(character))
