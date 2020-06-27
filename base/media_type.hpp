8ed033418938a5c6d550cac2e4ccbcbc2d8efe27
Tristan <code@thewoosh.me>
2020-06-27 16:59:32 +0200

    [MediaType] Introduce better data structure

--- base/media_type.hpp
+++ base/media_type.hpp
@@ -12,13 +12,27 @@
 
 #include "io/file.hpp"
 
+struct MediaType {
+	const std::string type;
+	const std::string subtype;
+	const std::string completeType;
+	const bool includeCharset;
+
+	inline MediaType(const std::string &type, const std::string &subtype, bool includeCharset)
+		: type(type), subtype(subtype), completeType(type + '/' + subtype), includeCharset(includeCharset) {
+	}
+
+	inline MediaType(const std::string &type, const std::string &subtype) : MediaType(type, subtype, type == "text") {
+	}
+};
+
 class MediaTypeFinder {
 public:
 	MediaTypeFinder() noexcept;
 
-	[[nodiscard]] const std::string &
+	[[nodiscard]] const MediaType &
 	DetectMediaType(const std::unique_ptr<IO::File> &) const noexcept;
 
 private:
-	std::map<std::string, std::string> mediaTypes;
+	std::map<std::string, MediaType> mediaTypes;
 };
