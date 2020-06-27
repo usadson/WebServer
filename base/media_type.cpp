3c5bc62ba5ca877eb074e70238d99463d91694b3
Tristan <code@thewoosh.me>
2020-06-27 16:12:16 +0200

    [MediaTypeFinder] Add more types and improve docs

--- base/media_type.cpp
+++ base/media_type.cpp
@@ -7,11 +7,23 @@
 #include "media_type.hpp"
 
 #include <algorithm>
+#include <sstream>
+
+#include "base/logger.hpp"
 
 static const std::string genericType = "application/octet-stream";
 
 MediaTypeFinder::MediaTypeFinder() noexcept : mediaTypes({
+	// NOTE This list must be kept minimal. There is no need to have every
+	//      extension in this list. It will only cause overhead and provide no
+	//      real advantage.
+	//
+	// If you need to have an extension in this list, you can add it, but keep
+	// the advice above in mind.
 	{ "html", "text/html" },
+	{ "json", "application/json" },
+	{ "jpg",  "image/jpeg" },
+	{ "png",  "image/png" },
 }) {
 }
 
@@ -21,8 +33,12 @@ MediaTypeFinder::DetectMediaType(const std::unique_ptr<IO::File> &file) const no
 
 	do {
 		auto dot = string.find_first_of('.');
-		if (dot == std::string::npos)
+		if (dot == std::string::npos) {
+			std::stringstream info;
+			info << "Unrecognized extension: \"" << file->Path() << '"';
+			Logger::Debug("MediaTypeFinder", info.str());
 			return genericType;
+		}
 		string = string.substr(dot + 1);
 
 		auto result = mediaTypes.find(string);
