f2b1fe4f522f1c68690a080734568aae63f79a6f
Tristan <code@thewoosh.me>
2020-06-28 20:00:43 +0200

    [Strings] Initial Commit

---
+++ base/strings.cpp
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include "strings.hpp"
+
+namespace Strings {
+
+const char *DefaultWebPage = "<!doctype html><html lang=\"en\"><head><meta name=\"author\" content=\"Tristan\"><meta charset=\"utf-8\"><meta name=\"description\" content=\"It works! This is a default webpage.\"><title>Wizard Web Server - Default Webpage</title><style>*{font-family:\"Noto Sans\",\"Calibri\",\"Roboto\",sans-serif}html{background-color:#ddd;margin:0;height:100%}body{height:100%;margin:0}main{margin:0 10%;height:100%;width:80%;background-color:#fff}h1{padding-top:10%;margin:0;text-align:center}div{margin:0 6%}</style></head><body><main><h1>Wizard Web Server</h1><div><h3>It works!</h3><p>The fact that you can read this means that the web server has been configured correctly!</p><p>Now go ahead and change this page to your personal homepage :)</p></div><div><h3>About</h3><p>This is a default page, served to you by the <a href=\"https://github.com/usadson/WebServer\">Wizard Web Server</a>, a free BSD-3-Clause licensed webserver.</p></div></main></body></html>";
+
+} // namespace Strings
---
+++ base/strings.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+namespace Strings {
+
+extern const char *DefaultWebPage;
+
+} // namespace Strings
