2c74a076e988d8e5bfbf1583fe97dd4b52abba41
Tristan <code@thewoosh.me>
2020-07-04 15:36:22 +0200

    [Logger] Use STL for loggable content

--- base/logger.hpp
+++ base/logger.hpp
@@ -13,33 +13,39 @@ namespace Logger {
 
 static const char *loggerEndStr = "\x1b[0m\n";
 
+template <typename T>
 inline void
-Debug(const std::string &source, const std::string &message) {
+Debug(const std::string &source, const T &message) {
 	std::cout << "[\x1b[34mDebug\x1b[0m] [\x1b[34m" << source << "\x1b[0m] \x1b[35m" << message << loggerEndStr;
 }
 
+template <typename T>
 inline void
-Error(const std::string &source, const std::string &message) {
+Error(const std::string &source, const T &message) {
 	std::clog << "[\x1b[34mError\x1b[0m] [\x1b[34m" << source << "\x1b[0m] \x1b[31m" << message << loggerEndStr;
 }
 
+template <typename T>
 inline void
-Info(const std::string &source, const std::string &message) {
+Info(const std::string &source, const T &message) {
 	std::cout << "[\x1b[34mInfo\x1b[0m] [\x1b[34m" << source << "\x1b[0m] \x1b[32m" << message << loggerEndStr;
 }
 
+template <typename T>
 inline void
-Log(const std::string &source, const std::string &message) {
+Log(const std::string &source, const T &message) {
 	std::cout << "[\x1b[34mLog\x1b[0m] [\x1b[34m" << source << "\x1b[0m] \x1b[37m" << message << loggerEndStr;
 }
 
+template <typename T>
 inline void
-Severe(const std::string &source, const std::string &message) {
+Severe(const std::string &source, const T &message) {
 	std::clog << "[\x1b[34mSevere\x1b[0m] [\x1b[34m" << source << "\x1b[0m] \x1b[31m" << message << loggerEndStr;
 }
 
+template <typename T>
 inline void
-Warning(const std::string &source, const std::string &message) {
+Warning(const std::string &source, const T &message) {
 	std::clog << "[\x1b[34mWarning\x1b[0m] [\x1b[34m" << source << "\x1b[0m] \x1b[33m" << message << loggerEndStr;
 }
 
