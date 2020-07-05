cbaecaaf494cd85d27e8505b27814a2233ad21ac
Tristan <code@thewoosh.me>
2020-07-05 20:40:22 +0200

    [HTTP] (Client) Add noexcept keyword to HTTP::Client::RecoverErrorTooManyRequestsPerThisConnection

--- http/client.hpp
+++ http/client.hpp
@@ -120,7 +120,7 @@ private:
 
 	// Handles TOO_MANY_REQUESTS_PER_THIS_CONNECTION. Called by RecoverError.
 	[[nodiscard]] bool
-	RecoverErrorTooManyRequestsPerThisConnection();
+	RecoverErrorTooManyRequestsPerThisConnection() noexcept;
 
 	// Clears the 'currentRequest' member variable.
 	void
