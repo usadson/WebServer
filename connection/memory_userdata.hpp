b379ce3544af2d43930c1753f44dbedd3e31c226
Tristan <code@thewoosh.me>
2020-07-07 21:05:40 +0200

    [Connection] Add MemoryUserData as internal userData for new memory_connection.cpp

---
+++ connection/memory_userdata.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include <iosfwd>
+
+struct MemoryUserData {
+	std::vector<char> input;
+	std::vector<char> output;
+	bool writeSendFile;
+};
