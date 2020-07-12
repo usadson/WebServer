ceb9b90aa598093ee1d165c111239d92de30aa3a
Tristan <code@thewoosh.me>
2020-07-12 19:23:39 +0200

    [CGI] (Script) Initial Commit

---
+++ cgi/script.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include <string>
+
+namespace CGI {
+
+struct Script {
+	std::string Command;
+	std::string Name;
+}
+
+} // namespace CGI
