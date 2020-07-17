90a17883974b458562159bf07a2b5e411451c26d
Tristan <code@thewoosh.me>
2020-07-17 14:27:38 +0200

    [Security] (tls_configuration.cpp) Initial Commit

---
+++ security/tls_configuration.cpp
@@ -0,0 +1,12 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include "tls_configuration.hpp"
+
+bool
+Security::TLSConfiguration::CreateContext() {
+	return true;
+}
