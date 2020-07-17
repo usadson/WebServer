2fa41f863bcc9867ef86c1178ba9906dbadde66e
Tristan <code@thewoosh.me>
2020-07-17 14:44:17 +0200

    [Security] (TLSConfiguration) Implement CreateContext

--- security/tls_configuration.cpp
+++ security/tls_configuration.cpp
@@ -6,7 +6,34 @@
 
 #include "tls_configuration.hpp"
 
+#define TLS_LIBRARY_OPENSSL
+
+#if defined(TLS_LIBRARY_OPENSSL)
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#else
+#error Unsupported TLS Library
+#endif
+
 bool
 Security::TLSConfiguration::CreateContext() {
+	SSL_load_error_strings();
+	OpenSSL_add_ssl_algorithms();
+
+	auto ctx = SSL_CTX_new(SSLv23_method());
+	this->context = ctx;
+
+	if (SSL_CTX_use_certificate_file(ctx, CertificateFile.c_str(), SSL_FILETYPE_PEM) != 1) {
+		ERR_print_errors_fp(stderr);
+		return false;
+	}
+
+	if (SSL_CTX_use_PrivateKey_file(ctx, PrivateKeyFile.c_str(), SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stderr);
+		return 0;
+	}
+
+	SSL_CTX_set_min_proto_version(ctx, TLS1_2_VERSION);
+
 	return true;
 }
