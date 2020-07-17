2c90ee80aa967eb8c9d2e63a0607ab0e5556357b
Tristan <code@thewoosh.me>
2020-07-18 01:11:01 +0200

    [Connection] Use new SecurityInternals proxy

--- connection/connection.cpp
+++ connection/connection.cpp
@@ -37,31 +37,8 @@
 #endif
 
 #include "base/logger.hpp"
+#include "connection/security_internals.hpp"
 #include "http/configuration.hpp"
-
-#ifdef TLS_LIBRARY_OPENSSL
-static const char *
-GetSSLErrorString(int error) {
-	switch (error) {
-		case SSL_ERROR_NONE:
-			return "SSL_ERROR_NONE";
-		case SSL_ERROR_ZERO_RETURN:
-			return "SSL_ERROR_ZERO_RETURN";
-		case SSL_ERROR_WANT_READ:
-			return "SSL_ERROR_WANT_READ or WRITE";
-		case SSL_ERROR_WANT_CONNECT:
-			return "SSL_ERROR_WANT_CONNECT or ACCEPT";
-		case SSL_ERROR_WANT_X509_LOOKUP:
-			return "SSL_ERROR_WANT_X509_LOOKUP";
-		case SSL_ERROR_SYSCALL:
-			return "SSL_ERROR_SYSCALL";
-		case SSL_ERROR_SSL:
-			return "SSL_ERROR_SSL";
-		default:
-			return "no string for this OpenSSL error code";
-	}
-}
-#endif
 
 Connection::~Connection() noexcept {
 	if (hasWriteFailed) {
@@ -71,11 +48,7 @@ Connection::~Connection() noexcept {
 	}
 
 	if (useTransportSecurity && securityContext != nullptr) {
-#if defined(TLS_LIBRARY_OPENSSL)
-		auto ssl = reinterpret_cast<SSL *>(securityContext);
-		SSL_shutdown(ssl);
-		SSL_free(ssl);
-#endif
+		ConnectionSecureInternals::Destruct(this);
 	}
 
 	// This makes sure all data has been transferred before closing the
@@ -102,55 +75,7 @@ Connection::Setup(const HTTP::Configuration &configuration) noexcept {
 	}
 
 	if (useTransportSecurity) {
-#if defined(TLS_LIBRARY_OPENSSL)
-		auto ctx = SSL_new(reinterpret_cast<SSL_CTX *>(configuration.tlsConfiguration.context));
-		securityContext = ctx;
-		if (securityContext == nullptr) {
-			ERR_print_errors_fp(stderr);
-			std::stringstream error;
-			error << "SSL_new failed. TLS context is " << configuration.tlsConfiguration.context;
-			Logger::Error(__PRETTY_FUNCTION__, error.str());
-			return false;
-		}
-
-		if (SSL_set_fd(ctx, internalSocket) == 0) {
-			ERR_print_errors_fp(stderr);
-			Logger::Error("Connection::Setup", "Failed to set socket (FD)");
-			return false;
-		}
-
-		int status = SSL_accept(ctx);
-		if (status != 1) {
-			ERR_print_errors_fp(stderr);
-			Logger::Error("Connection::Setup", "Failed to setup TLS communication.");
-			std::stringstream error;
-			error << "This is what OpenSSL has to say about it: \"";
-			error << GetSSLErrorString(SSL_get_error(ctx, status)) << '"';
-			Logger::Error("Connection::Setup", error.str());
-			error = std::stringstream();
-			error << "This is what error has to say about it: \"";
-			error << std::strerror(errno) << '"';
-			Logger::Error("Connection::Setup", error.str());
-			return false;
-		}
-
-		status = SSL_do_handshake(ctx);
-		if (status != 1) {
-			ERR_print_errors_fp(stderr);
-			Logger::Error("Connection::Setup", "Failed to perform TLS handshake.");
-			std::stringstream error;
-			error << "This is what OpenSSL has to say about it: \"";
-			error << GetSSLErrorString(SSL_get_error(ctx, status)) << '"';
-			Logger::Error("Connection::Setup", error.str());
-			error = std::stringstream();
-			error << "This is what error has to say about it: \"";
-			error << std::strerror(errno) << '"';
-			Logger::Error("Connection::Setup", error.str());
-			return false;
-		}
-
-		return true;
-#endif
+		return ConnectionSecureInternals::Setup(this, configuration);
 	}
 
 	// No setup is needed for non-secure connections
@@ -165,20 +90,7 @@ Connection::ReadChar(char *buf) const noexcept {
 	}
 
 	if (useTransportSecurity) {
-#if defined(TLS_LIBRARY_OPENSSL)
-		auto status = SSL_read(reinterpret_cast<SSL *>(securityContext), buf, 1);
-		if (status != 1) {
-			std::stringstream error;
-			error << "SSL_read failed. securityContext is " << securityContext;
-			Logger::Error(__FUNCTION__, error.str());
-			ERR_print_errors_fp(stderr);
-			return false;
-		} else {
-			return true;
-		}
-#else
-		return false;
-#endif
+		return ConnectionSecureInternals::ReadChar(this, buf);
 	}
 
 	return read(internalSocket, buf, 1) != -1;
@@ -188,27 +100,7 @@ bool
 Connection::SendFile(int fd, std::size_t count) noexcept {
 	if (useTransportSecurity) {
 		// TODO Use ktls for in-kernel TLS (= support for sendfile)
-#if defined(TLS_LIBRARY_OPENSSL)
-		std::array<char, 4096> buffer {};
-		do {
-			ssize_t result = read(fd, buffer.data(), 4096);
-			if (result == -1) {
-				return false;
-			}
-			count -= result;
-			do {
-				ssize_t writeResult = SSL_write(reinterpret_cast<SSL *>(securityContext), buffer.data(), result);
-				if (writeResult == -1) {
-					hasWriteFailed = true;
-					return false;
-				}
-				result -= writeResult;
-			} while (result != 0);
-		} while (count != 0);
-#else
-		hasWriteFailed = true;
-		return false;
-#endif
+		return ConnectionSecureInternals::SendFile(this, fd, count);
 	}
 
 #if defined(__FreeBSD__)
@@ -270,9 +162,7 @@ Connection::WriteString(const std::string &str) noexcept {
 	while (len != 0) {
 		ssize_t status;
 		if (useTransportSecurity) {
-#if defined(TLS_LIBRARY_OPENSSL)
-			status = SSL_write(reinterpret_cast<SSL *>(securityContext), str.c_str() + off, len);
-#endif
+			status = ConnectionSecureInternals::Write(this, str.c_str() + off, len);
 		} else {
 			status = write(internalSocket, str.c_str() + off, len);
 		}
@@ -297,9 +187,7 @@ Connection::WriteStringView(const std::string_view &str) noexcept {
 	while (len != 0) {
 		ssize_t status;
 		if (useTransportSecurity) {
-#if defined(TLS_LIBRARY_OPENSSL)
-			status = SSL_write(reinterpret_cast<SSL *>(securityContext), str.data() + off, len);
-#endif
+			status = ConnectionSecureInternals::Write(this, str.data() + off, len);
 		} else {
 			status = write(internalSocket, str.data() + off, len);
 		}
