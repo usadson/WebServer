4083879081ac18d81098fed39d98c0fb78ac6b4a
Tristan <code@thewoosh.me>
2020-07-18 01:10:03 +0200

    [Connection] (SecurityInternals) Initial Commit

---
+++ connection/security_internals.hpp
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+#include "connection/connection.hpp"
+#include "http/configuration.hpp"
+
+#include <cstddef> // for std::size_t
+
+namespace ConnectionSecureInternals {
+
+void
+Destruct(Connection *connection);
+
+bool
+Setup(Connection *connection, const HTTP::Configuration &configuration);
+
+bool
+ReadChar(const Connection *connection, char *buf);
+
+bool
+SendFile(Connection *connection, int fd, std::size_t count);
+
+int
+Write(Connection *connection, const char *str, std::size_t len);
+
+} // namespace ConnectionSecureInternals
