d02700fc923e9fd6526ce658f3890d1d1f7b1252
Tristan <code@thewoosh.me>
2020-07-18 13:44:54 +0200

    [main] Define ciphers from Mozilla SSL Configuration Generator

--- main.cpp
+++ main.cpp
@@ -88,5 +88,15 @@ LoadTLSConfiguration(Security::TLSConfiguration &config) {
 		return false;
 	}
 
+	config.cipherList =
+		"ECDHE-ECDSA-AES128-GCM-SHA256:"
+		"ECDHE-RSA-AES128-GCM-SHA256:"
+		"ECDHE-ECDSA-AES256-GCM-SHA384:"
+		"ECDHE-RSA-AES256-GCM-SHA384:"
+		"ECDHE-ECDSA-CHACHA20-POLY1305:"
+		"ECDHE-RSA-CHACHA20-POLY1305:"
+		"DHE-RSA-AES128-GCM-SHA256:"
+		"DHE-RSA-AES256-GCM-SHA384";
+
 	return config.CreateContext() && config.context != nullptr;
 }
