3d01f5ebd1fbb98efbcb39db9fd7019a56bcfb1a
Tristan <code@thewoosh.me>
2020-07-20 00:55:36 +0200

    [HTTP] (Client) Only send HSTS over HTTPS

--- http/client.cpp
+++ http/client.cpp
@@ -531,7 +531,7 @@ Client::SendMetadata(const std::string_view &response, std::size_t contentLength
 	metadata << "\r\nServer: " << server->config().serverProductName;
 	metadata << "\r\nConnection: " << (persistentConnection ? "keep-alive" : "close");
 
-	if (!server->config().hsts.empty()) {
+	if (server->config().useTransportSecurity && !server->config().hsts.empty()) {
 		metadata << "\r\nStrict-Transport-Security: " << server->config().hsts;
 	}
 
