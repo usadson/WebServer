8e8f9aee2e5b551b81f7f763de8b5e5fb4a8c4d0
Tristan <code@thewoosh.me>
2020-07-30 00:58:53 +0200

    [POSIX] Add stat.hpp

---
+++ posix/stat.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * This file is licensed under the BSD 2-Clause license.
+ * See the COPYING file for licensing information.
+ */
+
+/**
+ * This file is for code organizing. Instead of having these C libraries in
+ * default namespace, put them inside a proper namespace.
+ *
+ * We can't use the namespace 'posix', since it is probihited by the C++
+ * standard. It is rather stupid, since it is reserved for years but hasn't been
+ * used anywhere official AFAIK. Therefore, we'll use 'libposix'.
+ */
+
+#include <sys/stat.h>
+
+namespace psx {
+
+	using ::fstat;
+
+} // namespace psx
